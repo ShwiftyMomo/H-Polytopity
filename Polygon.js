2877120f01a5b8d96f79dc8e6ea5bda239973fc9
Milo_the_AI <milo@tacocat.com>
2021-05-04 14:15:43 -0700

    Added basic framework for site
    Made so there is html and javascript that can run; All content is placeholder

---
+++ Polygon.js
@@ -0,0 +1,11 @@
+class Polygon {
+    constructor(vertexNum) {
+
+        this.x=712;
+        this.y=290;
+        this.size=vertexNum;
+        this.color = [random(1,255),random(1,255),random(1,255)];
+
+    }
+  
+  }
